41cdfa57013c64fb7d13e0aad2d8262dad141c57
Claudio Familia Morel <claudio.familia.morel@gmail.com>
2022-03-25 21:08:53 -0400

    Exercise one complete

--- Allfiles/Mod04/Labfiles/Starter/Exercise 1/GradesPrototype/Data/Grade.cs
+++ Allfiles/Mod04/Labfiles/Starter/Exercise 1/GradesPrototype/Data/Grade.cs
@@ -13,8 +13,28 @@ namespace GradesPrototype.Data
     // WPF Databinding requires properties
 
     // TODO: Exercise 1: Task 1a: Convert Grade into a class and define constructors
-    public struct Grade
-    {
+    // DONE
+    public class Grade
+    {        
+        public Grade(int studentID, string assessmentDate, string subject, string
+        assessment, string comments)
+        {
+            StudentID = studentID;
+            AssessmentDate = assessmentDate;
+            SubjectName = subject;
+            Assessment = assessment;
+            Comments = comments;
+        }
+        
+        public Grade()
+        {
+            StudentID = 0;
+            AssessmentDate = DateTime.Now.ToString("d");
+            SubjectName = "Software";
+            Assessment = "A";
+            Comments = String.Empty;
+        }
+
         public int StudentID { get; set; }
         public string AssessmentDate { get; set; }
         public string SubjectName { get; set; }
@@ -23,24 +43,94 @@ namespace GradesPrototype.Data
     }
 
     // TODO: Exercise 1: Task 2a: Convert Student into a class, make the password property write-only, add the VerifyPassword method, and define constructors
-    public struct Student
+    // DONE 
+    public class Student
     {
         public int StudentID { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; }        
         public int TeacherID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        
+        public Student(int studentID, string userName, string password, string firstName, string lastName, int teacherID)
+        {
+            StudentID = studentID;
+            UserName = userName;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            TeacherID = teacherID;
+        }
+       
+        public Student()
+        {
+            StudentID = 0;
+            UserName = String.Empty;
+            Password = String.Empty;
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            TeacherID = 0;
+        }
+
+        private string _password = Guid.NewGuid().ToString();
+
+        public string Password
+        {
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public bool VerifyPassword(string pass)
+        {
+            return (String.Compare(pass, _password) == 0);
+        }
     }
 
     // TODO: Exercise 1: Task 2b: Convert Teacher into a class, make the password property write-only, add the VerifyPassword method, and define constructors
-    public struct Teacher
+    // DONE
+    public class Teacher
     {
         public int TeacherID { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; }        
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Class { get; set; }
+        
+        public Teacher(int teacherID, string userName, string password, string firstName, string lastName, string className)
+        {
+            TeacherID = teacherID;
+            UserName = userName;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Class = className;
+        }
+        
+        public Teacher()
+        {
+            TeacherID = 0;
+            UserName = String.Empty;
+            Password = String.Empty;
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            Class = String.Empty;
+        }
+
+        private string _password = Guid.NewGuid().ToString(); // Generate a random password by default
+
+        public string Password
+        {
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public bool VerifyPassword(string pass)
+        {
+            return (String.Compare(pass, _password) == 0);
+        }
     }
 }
